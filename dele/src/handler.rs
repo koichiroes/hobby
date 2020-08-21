d38ac66491ccc58156eb2d7ef5436d8a3319bbfe
koichiro <koichiro.okamoto.0805@gmail.com>
2020-08-21 23:57:18 +0900

    Delete files match specific patterns

--- dele/src/handler.rs
+++ dele/src/handler.rs
@@ -1,53 +1,38 @@
-use dirs::home_dir;
-use globset::Glob;
+use globset::{Glob, GlobSet, GlobSetBuilder};
 use std::fs::remove_file;
 use std::io;
 use std::path::{Path, PathBuf};
 
-trait Handler {
-  fn delete_file(self, filename: String, file_patterns: Vec<String>) -> io::Result<()>;
+pub(crate) trait Handler {
+  fn delete_file(&self, filename: &str) -> io::Result<()>;
 }
 
-struct SimpleHandler {
-  base_path: PathBuf,
+pub(crate) struct SimpleHandler {
+  set: GlobSet,
 }
 
 impl SimpleHandler {
-  fn new(base_dir: String) -> Self {
-    let mut base_path = PathBuf::from(base_dir.to_owned());
-    if base_dir.starts_with("~") {
-      base_path = get_home_dir().to_path_buf();
-      base_path.push(Path::new(base_dir.trim_start_matches("~")));
+  pub fn new(base_path: PathBuf, patterns: Vec<String>) -> Self {
+    let mut builder = GlobSetBuilder::new();
+    for pattern in &patterns {
+      let mut path = base_path.to_owned();
+      path.push(Path::new("**"));
+      path.push(Path::new(pattern));
+      builder.add(Glob::new(path.to_str().unwrap()).unwrap());
     }
-    SimpleHandler { base_path }
+    let set = builder.build().unwrap();
+    SimpleHandler { set }
   }
 }
 
 impl Handler for SimpleHandler {
-  fn delete_file(self, filename: String, file_patterns: Vec<String>) -> io::Result<()> {
-    for file_pattern in file_patterns {
-      let mut path = self.base_path.to_owned();
-      path.push(Path::new(file_pattern.as_str()));
-      match path.to_str() {
-        Some(pattern) => {
-          let glob = match Glob::new(pattern) {
-            Ok(g) => g.compile_matcher(),
-            Err(_) => panic!("failed to create glob pattern"),
-          };
-          if glob.is_match(filename.as_str()) {
-            remove_file(filename.to_owned())?;
-          }
-        }
-        None => panic!("{:?} is not valid path string", path),
+  fn delete_file(&self, filename: &str) -> io::Result<()> {
+    if self.set.is_match(filename) {
+      match remove_file(filename) {
+        Ok(_) => println!("{} is deleted", filename),
+        Err(e) => println!("failed to delete {}, {}", filename, e),
       }
     }
     Ok(())
-  }
-}
-
-fn get_home_dir() -> PathBuf {
-  match home_dir() {
-    Some(path) => path,
-    None => panic!("failed to get home directory"),
   }
 }
